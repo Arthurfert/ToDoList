b6101377276bfe29c507980368f8d687f5fe13aa
fert.arthur <fert.arthur83@gmail.com>
2025-04-10 09:52:30 +0200

    Upload files and update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ToDoList
-A simple TodoList website
+A simple TodoList website, with no memory, written with HTML, CSS and mainly JavaScript.
+
+## Features
+You can add and delete an object of your ToDo list, as well as making it "done".
+
+## Author
+This project was fully coded by **Arthur FERT**.
+
+**It doesn't have any copyrights**, as it is a very basic project.
---
+++ script.js
@@ -0,0 +1,73 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+document.addEventListener('DOMContentLoaded', function() {
+    var list = document.querySelector('ul');
+    
+    if (list) {
+      list.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+          ev.target.classList.toggle('checked');
+        }
+      }, false);
+    } else {
+      console.log("The <ul> element was not found.");
+    }
+  });
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var li = this.parentElement;
+      li.style.display = "none";
+    }
+  }
+}
+// Appeal the function when press enter to create an element
+var input = document.getElementById("myInput");
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("addBtn").click();
+  }
+});
